164efec3d6c1a24c8d39f9d3e9dbef5e50e3371c
Kedasu Manideep <quic_kmanidee@quicinc.com>
2025-07-04 22:47:22 +0530

    parsers: gpuinfo: Add support to extract GPU data from minidump
    This patch adds support in the gpuinfo parser to extract GPU-related
    information from minidump files. The enhancement enables automated
    retrieval of GPU state and diagnostics, which is useful for debugging GPU
    crashes and anomalies in post-mortem analysis.
    
    Change-Id: I75de30fe57768f602a4da73cf992516dd8499d63

--- linux-ramdump-parser-v2/parsers/gpu/gpuinfo_510.py
+++ linux-ramdump-parser-v2/parsers/gpu/gpuinfo_510.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2021 The Linux Foundation. All rights reserved.
-# Copyright (c) 2022-2025, Qualcomm Innovation Center, Inc. All rights reserved.
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 and
@@ -21,95 +21,8 @@ from parsers.gpu.gmu_info import generate_gmu_t32_files
 from parsers.gpu.gpu_snapshot import create_snapshot_from_ramdump
 from parsers.gpu.gpu_snapshot import extract_gmu_mem_from_snapshot
 from parsers.gpu.gpu_eventlog import parse_eventlog_buffer
+from parsers.gpu.gpuinfo_common import *
 from print_out import print_out_str
-
-
-# Global Configurations
-ADRENO_DISPATCH_DRAWQUEUE_SIZE = 128
-KGSL_DEVMEMSTORE_SIZE = 40
-KGSL_PRIORITY_MAX_RB_LEVELS = 4
-KGSL_MAX_POOLS = 6
-PAGE_SIZE = 4096
-
-KGSL_CACHEMODE_MASK = 0x0C000000
-KGSL_CACHEMODE_SHIFT = 26
-KGSL_MEMALIGN_MASK = 0x00FF0000
-KGSL_MEMALIGN_SHIFT = 16
-KGSL_MEMTYPE_MASK = 0x0000FF00
-KGSL_MEMTYPE_SHIFT = 8
-
-KGSL_CONTEXT_SECURE = 0x00020000
-
-KGSL_MEMDESC_GLOBAL = (1 << 1)
-KGSL_MEMDESC_SECURE = (1 << 4)
-KGSL_MEMDESC_PRIVILEGED = (1 << 6)
-KGSL_MEMDESC_UCODE = (1 << 7)
-KGSL_MEMDESC_RANDOM = (1 << 8)
-
-KGSL_MEMFLAGS_GPUREADONLY = (1 << 24)
-KGSL_MEMFLAGS_USE_CPU_MAP = (1 << 28)
-KGSL_MEMFLAGS_VBO = (1 << 34)
-
-VRB_PREEMPT_COUNT_TOTAL_L0_IDX = 6
-VRB_PREEMPT_COUNT_TOTAL_L1A_IDX = 7
-VRB_PREEMPT_COUNT_TOTAL_L1B_IDX = 8
-
-KGSL_HFI_MEMKIND_SCRATCH = 0x2
-
-MAX_NUM_GC_RBS = 4
-MAX_NUM_LPAC_RBS = 1
-MAX_NUM_RBS = (MAX_NUM_GC_RBS + MAX_NUM_LPAC_RBS)
-RB_SCRATCH_DWORDS_SZ = 5
-
-kgsl_cachemode = ['-', 'u', 't', 'b']
-
-kgsl_ctx_type = ['ANY', 'GL', 'CL', 'C2D', 'RS', 'VK']
-
-kgsl_ctx_priv = [
-    ((1 << 0), 's', 'submitted'),           # KGSL_CONTEXT_PRIV_SUBMITTED
-    ((1 << 1), 'd', 'detached'),            # KGSL_CONTEXT_PRIV_DETACHED
-    ((1 << 2), 'i', 'invalid'),             # KGSL_CONTEXT_PRIV_INVALID
-    ((1 << 3), 'p', 'pagefault'),           # KGSL_CONTEXT_PRIV_PAGEFAULT
-    ((1 << 16), 'F', 'Fault'),              # ADRENO_CONTEXT_FAULT
-    ((1 << 17), 'H', 'GPU Hang'),           # ADRENO_CONTEXT_GPU_HANG
-    ((1 << 18), 'T', 'GPU Hang FT'),        # ADRENO_CONTEXT_GPU_HANG_FT
-    ((1 << 19), 'E', 'Skip EOF'),           # ADRENO_CONTEXT_SKIP_EOF
-    ((1 << 20), 'P', 'Force Preamble'),     # ADRENO_CONTEXT_FORCE_PREAMBLE
-    ((1 << 21), 'C', 'Skip CMD'),           # ADRENO_CONTEXT_SKIP_CMD
-    ((1 << 22), 'L', 'Fence Log')           # ADRENO_CONTEXT_FENCE_LOG
-]
-
-kgsl_memtype = [
-                'any(0)',
-                'framebuffer',
-                'renderbuffer',
-                'arraybuffer',
-                'elementarraybuffer',
-                'vertexarraybuffer',
-                'texture',
-                'surface',
-                'egl_surface',
-                'gl',
-                'cl',
-                'cl_buffer_map',
-                'cl_buffer_nomap',
-                'cl_image_map',
-                'cl_image_nomap',
-                'cl_kernel_stack',
-                'command',
-                '2d',
-                'egl_image',
-                'egl_shadow',
-                'egl_multisample',
-                'kernel'
-]
-
-adreno_preempt_state = ['NONE', 'START', 'TRIGGERED', 'FAULTED', 'PENDING',
-                        'COMPLETE']
-
-
-def strhex(x): return str(hex(x))
-def str_convert_to_kb(x): return str(x//1024) + 'kb'
 
 
 class GpuParser_510(RamParser):
@@ -507,14 +420,7 @@ class GpuParser_510(RamParser):
                      str(len(features_list)-1))
 
     def parse_kgsl_data(self, dump):
-        adreno_boolean_field = ['long_ib_detect', 'lm_enabled', 'acd_enabled',
-                                'hwcg_enabled', 'throttling_enabled',
-                                'sptp_pc_enabled', 'bcl_enabled',
-                                'clx_enabled', 'dcvs_profile_enabled',
-                                'dms_enabled', 'gmu_ab',
-                                'gpu_llc_slice_enable',
-                                'gpuhtw_llc_slice_enable', 'lpac_enabled',
-                                'perfcounter', ]
+        adreno_boolean_list = adreno_boolean_data + ['dcvs_profile_enabled']
         kgsl_generic_field = ['open_count', 'active_cnt', 'state',
                               'requested_state', 'speed_bin',
                               'reset_counter', ]
@@ -526,7 +432,7 @@ class GpuParser_510(RamParser):
                                               kgsl_field)
             self.writeln(f'{kgsl_field}: ' + str(value))
 
-        for adreno_field in adreno_boolean_field:
+        for adreno_field in adreno_boolean_list:
             addr = dump.struct_field_addr(self.devp,
                                           'struct adreno_device', adreno_field)
             value = dump.read_bool(addr)
@@ -843,13 +749,7 @@ class GpuParser_510(RamParser):
                                            str(queue[8])))
 
     def parse_pwrctrl_data(self, dump):
-        pwrctl_data = ['active_pwrlevel', 'previous_pwrlevel',
-                       'default_pwrlevel', 'thermal_pwrlevel',
-                       'thermal_time', 'power_flags', 'ctrl_flags',
-                       'rt_bus_hint', 'rt_pwrlevel_hint', 'num_pwrlevels',
-                       'min_pwrlevel', 'max_pwrlevel', 'bus_percent_ab',
-                       'bus_width', 'bus_ab_mbytes', 'cur_buslevel',
-                       'interval_timeout', ]
+        pwrctrl_list = pwrctrl_data + ['rt_pwrlevel_hint']
         pwrctl_data_hex = ['power_flags', 'ctrl_flags']
 
         pwrctrl_address = dump.struct_field_addr(self.devp,
@@ -862,7 +762,7 @@ class GpuParser_510(RamParser):
         self.writeln('pwrctrl_address:  ' + strhex(pwrctrl_address))
         self.writeln('bus_control: ' + str(bus_control))
 
-        for data in pwrctl_data:
+        for data in pwrctrl_list:
             value = dump.read_structure_field(pwrctrl_address,
                                               'struct kgsl_pwrctrl', data)
             if data == 'num_pwrlevels':
@@ -1137,13 +1037,6 @@ class GpuParser_510(RamParser):
         self.writeln(format_str.format('L1B', str(preempt_count_total_l1B)))
 
     def parse_dcvs_tunables(self, dump):
-        DCVS_Tunables_list = ['penalty_up', 'penalty_down',
-                              'first_step_down', 'subsequent_step_down',
-                              'min_freq_mhz', 'max_freq_mhz',
-                              'target_fps', 'num_samples_up',
-                              'num_samples_down', 'strict_frame',
-                              'non_linear_ramp_up', 'non_linear_ramp_down',
-                              'mod_percent', ]
         hwsched_addr = dump.struct_field_addr(self.devp,
                                               'struct adreno_device',
                                               'hwsched')
@@ -1164,13 +1057,7 @@ class GpuParser_510(RamParser):
                                           'struct adreno_hwsched',
                                           'flags')
         self.writeln('flags: ' + strhex(flags))
-        flags_list = ['ADRENO_HWSCHED_POWER', 'ADRENO_HWSCHED_ACTIVE',
-                      'ADRENO_HWSCHED_CTX_BAD_LEGACY',
-                      'ADRENO_HWSCHED_CONTEXT_QUEUE',
-                      'ADRENO_HWSCHED_HW_FENCE',
-                      'ADRENO_HWSCHED_FORCE_RETIRE_GMU',
-                      'ADRENO_HWSCHED_GPU_SOFT_RESET', ]
-        for i, flag in enumerate(flags_list):
+        for i, flag in enumerate(hwsched_flags_data):
             value = (flags >> i) & 1
             self.writeln(f'{flag}: ' + str(value))
 
@@ -1182,47 +1069,7 @@ class GpuParser_510(RamParser):
                                             'struct kgsl_memdesc', 'hostptr')
         size = dump.read_structure_field(memstore_obj,
                                          'struct kgsl_memdesc', 'size')
-        preempted = dump.read_s32(hostptr + 16)
-        current_context = dump.read_s32(hostptr + 32)
-
-        self.writeln("hostptr: " + strhex(hostptr))
-        self.writeln("current_context: " + str(current_context))
-        self.writeln("preempted: " + str(preempted) + " [Deprecated]")
-
-        def add_increment(x): return x + 4
-
-        self.writeln("\nrb contexts:")
-        format_str = '{0:^20} {1:^20} {2:^20} {3:^20}'
-        self.writeln(format_str.format("rb_index", "soptimestamp",
-                                       "eoptimestamp", "current_context"))
-
-        # Skip process contexts since their timestamps are
-        # displayed in open/active context sections
-        hostptr = hostptr + size - (5 * KGSL_DEVMEMSTORE_SIZE) - 8
-        for rb_id in range(KGSL_PRIORITY_MAX_RB_LEVELS):
-            soptimestamp = dump.read_s32(hostptr)
-            hostptr = add_increment(hostptr)
-            # skip unused entry
-            hostptr = add_increment(hostptr)
-            eoptimestamp = dump.read_s32(hostptr)
-            hostptr = add_increment(hostptr)
-            # skip unused entry
-            hostptr = add_increment(hostptr)
-            # skip preempted entry
-            hostptr = add_increment(hostptr)
-            # skip unused entry
-            hostptr = add_increment(hostptr)
-            # skip ref_wait_ts entry
-            hostptr = add_increment(hostptr)
-            # skip unused entry
-            hostptr = add_increment(hostptr)
-            current_context = dump.read_s32(hostptr)
-            hostptr = add_increment(hostptr)
-            # skip unused entry
-            hostptr = add_increment(hostptr)
-
-            self.writeln(format_str.format(str(rb_id), str(soptimestamp),
-                                           str(eoptimestamp), current_context))
+        parse_memstore_memory_common(dump, hostptr, size, self.writeln)
 
     def parse_fence_data(self, dump):
         context_idr = dump.struct_field_addr(self.devp,
